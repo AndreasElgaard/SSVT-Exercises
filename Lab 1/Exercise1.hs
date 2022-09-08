92e6295d33177ffa5306367c5da40d0438d91306
agnes.masip <agnes.masip@estudiantat.upc.edu>
2022-09-08 13:11:56 +0200

    square and cubic code done

--- Lab 1/Exercise1.hs
+++ Lab 1/Exercise1.hs
@@ -13,3 +13,17 @@ exercise1 n = leftPart n == rightPart n
 
 testExercise1:: Integer -> Property
 testExercise1 n = n > 0 ==> leftPart n == rightPart n
+
+--Now the cubic exercise.
+
+leftPart3:: Integer -> Integer
+leftPart3 n = sum[k^3 | k <- [1..n]] 
+
+rightPart3:: Integer -> Integer
+rightPart3 n = (n*(n+1) `div` 2)^2
+
+exercise1cub:: Integer -> Bool
+exercise1cub n = leftPart3 n == rightPart3 n
+
+testExercise1cub:: Integer -> Property
+testExercise1cub n = n > 0 ==> leftPart3 n == rightPart3 n
