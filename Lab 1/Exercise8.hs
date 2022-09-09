f15715a0206f73955fdec2bf196faf2b8314a888
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2022-09-09 11:42:49 +0200

    [LAB-1] Exercise 8

--- Lab 1/Exercise8.hs
+++ Lab 1/Exercise8.hs
@@ -2,7 +2,6 @@ module Exercise8 where
 import Data.List
 import Test.QuickCheck    
 
-
 data Boy = Matthew | Peter | Jack | Arnold | Carl
     deriving (Eq,Show)
     
@@ -10,14 +9,14 @@ boys :: [Boy]
 boys = [Matthew, Peter, Jack, Arnold, Carl]
 
 accuses ::  Boy -> Boy -> Bool
-accuses Peter Matthew = True
-accuses Peter Jack = True
-accuses Jack Peter = True
-accuses Jack Matthew = True
-accuses Arnold Matthew = True
-accuses Arnold Peter = True
-accuses Carl Arnold = True
-accuses _ _ = False
+Peter `accuses` Matthew = True
+Peter `accuses` Jack = True
+Jack `accuses` Matthew = True
+Jack `accuses` Peter = True
+Arnold `accuses` Matthew = True
+Arnold `accuses` Peter = True
+Carl `accuses` Arnold = True
+_ `accuses` _ = False
 
 accusers ::  Boy -> [Boy]
 accusers Matthew = [Peter, Jack, Arnold]
@@ -25,3 +24,12 @@ accusers Jack = [Peter]
 accusers Peter = [Jack]
 accusers Arnold = [Carl]
 accusers Carl = []
+
+matthewStatement = not (Matthew `accuses` Carl || Matthew `accuses` Matthew) --DeMorgan's law
+peterStatement = Peter `accuses` Matthew || Peter `accuses` Jack
+jackStatement = Jack `accuses` Matthew && Jack `accuses` Peter
+arnoldStatement = Arnold `accuses` Matthew /= Arnold `accuses` Peter
+carlStatement = (Carl `accuses` Arnold)
+
+-- honest ::  [Boy]
+-- honest = matthewStatement && peterStatement 
