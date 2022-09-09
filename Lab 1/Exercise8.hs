5827c2e4d7d6cc87a2e0a58bcfbbc7ddd9c62f2c
Ale <mifsud.aleandro@gmail.com>
2022-09-09 17:10:00 +0200

    Lots of progress with Ex8

--- Lab 1/Exercise8.hs
+++ Lab 1/Exercise8.hs
@@ -1,6 +1,5 @@
 module Exercise8 where
-import Data.List
-import Test.QuickCheck    
+import Data.List 
 
 data Boy = Matthew | Peter | Jack | Arnold | Carl
     deriving (Eq,Show)
@@ -25,21 +24,39 @@ accusers Peter = [Jack]
 accusers Arnold = [Carl]
 accusers Carl = []
 
-matthewStatement = not (Matthew `accuses` Carl || Matthew `accuses` Matthew) --DeMorgan's law
-peterStatement = Peter `accuses` Matthew || Peter `accuses` Jack
-jackStatement = not matthewStatement && not peterStatement
-arnoldStatement = matthewStatement /= peterStatement
-carlStatement = not arnoldStatement
+statement :: Boy -> Bool
+statement Matthew = not (Matthew `accuses` Carl || Matthew `accuses` Matthew) --DeMorgan's law
+statement Peter = Peter `accuses` Matthew || Peter `accuses` Jack
+statement Jack = not (statement Matthew) && not (statement Peter)
+statement Arnold = statement Matthew /= statement Peter
+statement Carl = not (statement Arnold)
 
+statementWBoy :: Boy -> (Boy, Bool)
+statementWBoy b = (b, statement b)
 -- Assuming that the statements provided by Jack and Arnold
 -- have contradictions within, we can deduct that their statements
 -- are false and hence Jack and Arnold prove to be not honest.
 
-statements :: [(Boy, Bool)]
-statements = [(Matthew, matthewStatement), (Peter, peterStatement), (Jack, jackStatement), (Arnold, arnoldStatement), (Carl, carlStatement)]
-
--- guilty, honest ::  [Boy]
+-- statements :: [(Boy, Bool)]
+-- statements = [(Matthew, matthewStatement), (Peter, peterStatement), (Jack, jackStatement), (Arnold, arnoldStatement), (Carl, carlStatement)]
 
 
+isHonest :: [(a, Bool)] -> [a] -> [a]
+isHonest [] outputArray = outputArray 
+isHonest ((boy, True):xs) outputArray  = isHonest xs (boy : outputArray)
+isHonest ((_, False):xs) outputArray  = isHonest xs outputArray 
+
+honest ::  [Boy]
+honest = isHonest tuplesArr []
+    where tuplesArr = map statementWBoy boys 
+
+
+guilty :: [Boy]
+guilty = boys 
+-- We know that the honest boys -> [Carl,Peter,Matthew]
+-- Therefor the liars are -> [Arnold, Jack]
+-- By checking the statements we see that Jack accusses Mathew and Peters statement, but they are telling the truth
+--      Matthews statement says that it was neither Matthew nor Carl
+--      Peters statement says it was either Matthew or Jack, by the premise that Matthews' statement is honest, Jack is the thief
 -- honest ::  [Boy]
 -- honest = matthewStatement && peterStatement 
