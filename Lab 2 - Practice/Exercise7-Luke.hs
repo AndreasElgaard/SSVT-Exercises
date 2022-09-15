249bd06839874414d90e86e7df8a394e0329a922
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2022-09-15 16:22:48 +0200

    [LAB-2] Exs: 4,7

--- Lab 2 - Practice/Exercise7-Luke.hs
+++ Lab 2 - Practice/Exercise7-Luke.hs
@@ -2,44 +2,35 @@ module Exercise7 where
 import Test.QuickCheck
 import Data.Char
 
-mod97Algorithm ibanInInt = (read ibanInInt) `mod` 97
-
-convertCharsToIntsIban iban = concatMap show (map letterToDigits iban)
-
-moveInitCharsToEndIban iban = amendedIban
-    where
-        amendedIban = shorterIban ++ firstFour
-        shorterIban = drop 4 iban
-        firstFour = take 4 iban
-
 letterToDigits :: Char -> Int
-letterToDigits 'A' = 10
-letterToDigits 'B' = 11
-letterToDigits 'C' = 12
-letterToDigits 'D' = 13
-letterToDigits 'E' = 14
-letterToDigits 'F' = 15
-letterToDigits 'G' = 16
-letterToDigits 'H' = 17
-letterToDigits 'I' = 18
-letterToDigits 'J' = 19
-letterToDigits 'K' = 20
-letterToDigits 'L' = 21
-letterToDigits 'M' = 22
-letterToDigits 'N' = 23
-letterToDigits 'O' = 24
-letterToDigits 'P' = 25
-letterToDigits 'Q' = 26
-letterToDigits 'R' = 27
-letterToDigits 'S' = 28
-letterToDigits 'T' = 29
-letterToDigits 'U' = 30
-letterToDigits 'V' = 31
-letterToDigits 'W' = 32
-letterToDigits 'X' = 33
-letterToDigits 'Y' = 34
-letterToDigits 'Z' = 35
-letterToDigits code = digitToInt code
+letterToDigits char = case char of
+ 'A' -> 10
+ 'B' -> 11
+ 'C' -> 12
+ 'D' -> 13
+ 'E' -> 14
+ 'F' -> 15
+ 'G' -> 16
+ 'H' -> 17
+ 'I' -> 18
+ 'J' -> 19
+ 'K' -> 20
+ 'L' -> 21
+ 'M' -> 22
+ 'N' -> 23
+ 'O' -> 24
+ 'P' -> 25
+ 'Q' -> 26
+ 'R' -> 27
+ 'S' -> 28
+ 'T' -> 29
+ 'U' -> 30
+ 'V' -> 31
+ 'W' -> 32
+ 'X' -> 33
+ 'Y' -> 34
+ 'Z' -> 35
+ code -> digitToInt code
 
 stateIbanCharacters :: Num p => [Char] -> p
 stateIbanCharacters state = case state of
@@ -123,8 +114,28 @@ stateIbanCharacters state = case state of
     "AE" -> 23
     "GB" -> 22
     "VG" -> 24
+    _ -> 0
+
+mod97Algorithm :: (Integral a, Read a) => String -> a
+mod97Algorithm ibanInInt = read ibanInInt `mod` 97
+
+convertCharsToIntsIban :: [Char] -> [Char]
+convertCharsToIntsIban iban = concatMap show (map letterToDigits iban)
+
+moveInitCharsToEndIban :: [a] -> [a]
+moveInitCharsToEndIban iban = amendedIban
+    where
+        amendedIban = shorterIban ++ firstFour
+        shorterIban = drop 4 iban
+        firstFour = take 4 iban
 
 iban :: String -> Bool
 iban val
-    | not (stateIbanCharacters (take 2 val) == length val) = False
-    | otherwise = True
+    | stateIbanCharacters (take 2 val) /= length val = False
+    | checkModulusIban val = True
+    | otherwise = False
+
+checkModulusIban val = modulus == 1 where 
+        modulus = mod97Algorithm convertedToIntIban
+        convertedToIntIban = convertCharsToIntsIban movedCharIban
+        movedCharIban = moveInitCharsToEndIban val 
