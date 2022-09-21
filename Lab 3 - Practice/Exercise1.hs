a2bb99da175b97aadc8dc33e8720c0b943fad729
agnes.masip <agnes.masip@estudiantat.upc.edu>
2022-09-21 15:53:33 +0200

    exercise 1 3 primers

---
+++ Lab 3 - Practice/Exercise1.hs
@@ -0,0 +1,21 @@
+import Lecture3
+
+--contradiction 
+contradiction :: Form -> Bool
+contradiction f = not (any (\ v -> evl v f) (allVals f))
+
+-- if any (\ v -> evl v f) (allVals f) then False else True
+
+tautology :: Form -> Bool
+tautology f = all (\ v -> evl v f) (allVals f)
+
+    -- | logical entailment 
+entails :: Form -> Form -> Bool
+entails p q 
+    | p == False = True
+    | p == True && q == True = True
+    | otherwise False
+
+    -- | logical equivalence
+equiv :: Form -> Form -> Bool
+equiv = undefined
