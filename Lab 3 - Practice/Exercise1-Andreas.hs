d8bf47c70d1179dcdc217a3b6c66ccd4e1fc61d6
Andreas Elgaard Sørensen <andreas.elgaard@outlook.com>
2022-09-21 15:05:52 +0200

    Added Lecture3, SetOrd and Exercise1-Andreas

---
+++ Lab 3 - Practice/Exercise1-Andreas.hs
@@ -0,0 +1,21 @@
+module Exercise1 where
+
+import Lecture3
+import Data.List
+import Data.Char
+import Test.QuickCheck
+
+
+contradiction :: Form -> Bool
+contradiction = undefined
+
+tautology :: Form -> Bool
+tautology = undefined
+    
+-- | logical entailment 
+entails :: Form -> Form -> Bool
+entails = undefined
+
+-- | logical equivalence
+equiv :: Form -> Form -> Bool
+equiv = undefined
