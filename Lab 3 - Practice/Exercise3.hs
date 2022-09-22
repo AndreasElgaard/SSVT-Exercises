2e6af19a02f64eb93277e775ea493c73c3ed87e7
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2022-09-22 14:34:18 +0200

    [LAB-3] Ex 3 Rename

--- Lab 3 - Practice/Exercise3.hs
+++ Lab 3 - Practice/Exercise3.hs
@@ -5,24 +5,11 @@ import           Data.List
 import           Lecture3
 import           Test.QuickCheck
 
-cnf :: Form -> Form
-cnf = nnf . arrowfree
-
--- test :: [Form] -> [Form]
--- test (Neg a : Cnj fs       : xs) = Dsj (map Neg fs) : test xs
--- test (Neg a : Dsj fs       : xs) = Cnj (map Neg fs) : test xs
--- test (Neg a : (Impl f1 f2) : xs) = Cnj [f1, Neg f2] : test xs
--- test []                          = []
--- test (x : xs)                    = x : test xs
-
-
--- CNJ ([Prop 1, DSJ ([Prop 2, Prop 3])])
--- DSJ(CNJ ([Prop 1, Prop 2]), CNJ ([Prop 1, Prop 3])))
 
 deMorgansHandler :: Form -> Form
 deMorgansHandler (Prop x      ) = Prop x
-deMorgansHandler (Neg  (Cnj a)) = Dsj (map (Neg . deMorgansHandler) a)
-deMorgansHandler (Neg  (Dsj a)) = Cnj (map (Neg . deMorgansHandler) a)
+deMorgansHandler (Neg  (Cnj a)) = Dsj (map (deMorgansHandler . Neg) a)
+deMorgansHandler (Neg  (Dsj a)) = Cnj (map (deMorgansHandler . Neg) a)
 deMorgansHandler (Cnj  a      ) = Cnj (map deMorgansHandler a)
 deMorgansHandler (Dsj  a      ) = Dsj (map deMorgansHandler a)
 deMorgansHandler (Neg (Impl f1 f2)) =
@@ -31,22 +18,24 @@ deMorgansHandler (Impl f1 f2) =
     Impl (deMorgansHandler f1) (deMorgansHandler f2)
 deMorgansHandler (Equiv f1 f2) =
     Equiv (deMorgansHandler f1) (deMorgansHandler f2)
-deMorgansHandler x = x
-
-deMorgansHandler1 :: Form -> Form
-deMorgansHandler1 (Prop x) = Prop x
-deMorgansHandler1 (Neg  f) = Neg (deMorgansHandler1 f)
-deMorgansHandler1 (Cnj  a) = Dsj (map (Neg . deMorgansHandler1) a)
-deMorgansHandler1 (Dsj  a) = Cnj (map (Neg . deMorgansHandler1) a)
-deMorgansHandler1 (Impl f1 f2) =
-    Cnj [deMorgansHandler1 f1, Neg (deMorgansHandler1 f2)]
-deMorgansHandler1 x = x
-
--- handler (Cnj a) f1 =
-
--- distributiveLawHandler :: Form -> Form
--- distributiveLawHandler (Cnj a) = map distributiveLawHandler a
--- distributiveLawHandler (Dsj a) = map
+deMorgansHandler (Neg (Neg (Prop a))) = Prop a
+deMorgansHandler (Neg a             ) = Neg (deMorgansHandler a)
 
 
 
+cnf :: p -> Form
+cnf f1 = deMorgened  where
+    -- formula =  Dsj createCnj
+    -- createCnj = map Cnj (createFormForProp mapOfListOfProps)
+    deMorgened        = deMorgansHandler negatedDisjuncted
+    negatedDisjuncted = Neg (Dsj conjuncted)
+    conjuncted        = map Cnj propsMap
+    propsMap          = map createFormForProp mapOfListOfProps
+    mapOfListOfProps  = map fst filteredFalseMap
+    filteredFalseMap  = filter (\(val, bool) -> not bool) $ mapOfEval
+    mapOfEval         = map (\n -> (n, evl n form2)) (allVals form2)
+
+createFormForProp ((prop, True) : xs) = Prop prop : createFormForProp (xs)
+createFormForProp ((prop, False) : xs) =
+    Neg (Prop prop) : createFormForProp (xs)
+createFormForProp [] = []
