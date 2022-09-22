40a5a74aea7256495d66286c1fb0a88fc708cb6c
agnes.masip <agnes.masip@estudiantat.upc.edu>
2022-09-22 17:05:35 +0200

    quickcheck

--- Lab 3 - Practice/Exercise5-Agnes.hs
+++ Lab 3 - Practice/Exercise5-Agnes.hs
@@ -14,6 +14,42 @@ sub f@(Dsj  [f1, f2]) = unionSet (unionSet (Set [f]) (sub f1)) (sub f2)
 sub f@(Impl  f1 f2  ) = unionSet (unionSet (Set [f]) (sub f1)) (sub f2)
 sub f@(Equiv f1 f2  ) = unionSet (unionSet (Set [f]) (sub f1)) (sub f2)
 
+
+instance Arbitrary Form where
+    arbitrary = frequency listOfArbs
+      where
+        arbProp = do
+            Prop . abs <$> arbitrary
+        arbNeg = do
+            x <- frequency listOfArbs
+            return $ Neg x
+        arbCnj = do
+            x <- frequency listOfArbs
+            y <- frequency listOfArbs
+            return $ Cnj [x, y]
+        arbDsj = do
+            x <- frequency listOfArbs
+            y <- frequency listOfArbs
+            -- vectorOf
+            return $ Dsj [x, y]
+        arbImpl = do
+            x <- frequency listOfArbs
+            y <- frequency listOfArbs
+            return $ Impl x y
+        arbEquiv = do
+            x <- frequency listOfArbs
+            y <- frequency listOfArbs
+            return $ Equiv x y
+        listOfArbs =
+            [ (10, arbProp)
+            , (2 , arbNeg)
+            , (1 , arbCnj)
+            , (3 , arbDsj)
+            , (4 , arbImpl)
+            , (1 , arbEquiv)
+            ]
+
+--quickCheck prop_checkSubContainsCorrectBaseProps
 prop_checkSubContainsCorrectBaseProps :: Form -> Bool
 prop_checkSubContainsCorrectBaseProps f1= all
     (== True)
@@ -35,5 +71,11 @@ prop_checkSubContainsFullFormula f1 = inSet f1 subF1  where
 prop_checkSubContainsIncorrectFullFormula :: Form -> Bool
 prop_checkSubContainsIncorrectFullFormula f1 = inSet (Neg f1) subF1  where
     subF1 = sub f1
+
+
+-- part 2
+
+nsub :: Form -> Int
+nsub f1 = undefined 
 
 
