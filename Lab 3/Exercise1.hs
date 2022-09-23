7c8df2ed7ca8a90f7fc839a10ced3ba9696996f5
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2022-09-23 14:01:52 +0200

    [LAB-3] Ex 1 Final

--- Lab 3/Exercise1.hs
+++ Lab 3/Exercise1.hs
@@ -36,31 +36,51 @@ equiv :: Form -> Form -> Bool
 equiv f1 f2 = map (`evl` f1) (allVals f1) == map (`evl` f2) (allVals f2)
 
 
+-- Verifying that A ^ NOT (A) is a contradiction, as per our contradiction function
+-- i.e. A ^ NOT (A) is ALWAYS False
 prop_validContradiction :: Bool
 prop_validContradiction = contradiction $ head $ parse "*(1 -1)"
 
+-- Verifying that A v NOT (A) is NOT a contradiction, as per our contradiction function
+-- i.e. A v NOT (A) is NOT ALWAYS False
 prop_invalidContradiction :: Bool
 prop_invalidContradiction = not (contradiction $ head $ parse "+(1 -1)")
 
+-- Verifying that A = A is an Equivalence, as per our equivalence function
+-- i.e. A = A for ALL true, false combinations
 prop_validEquiv :: Bool
 prop_validEquiv = f1 `equiv` f1 where f1 = head $ parse "*(1 -1)"
 
+-- Verifying that A != B is an Equivalence, as per our equivalence function
+-- i.e. A != B for SOME true, false combinations
 prop_invalidEquiv :: Bool
 prop_invalidEquiv = not (f1 `equiv` f2)  where
     f1 = head $ parse "*(1 -1)"
     f2 = head $ parse "+(1 2)"
 
+-- Verifying that A v NOT A is a tautology, as per our tautology function
+-- i.e. A v NOT A is TRUE for ALL true, false combinations
 prop_validTautology :: Bool
 prop_validTautology = tautology $ head $ parse "+(1 -1)"
 
+-- Verifying that A ^ NOT A is NOT a tautology, as per our tautology function
+-- i.e. A ^ NOT A is NOT TRUE for SOME true, false combinations
 prop_invalidTautology :: Bool
 prop_invalidTautology = not (tautology $ head $ parse "*(1 -1)")
 
+-- Verifying that A  --> A as per our entailment function
+-- i.e. TRUE for A implies TRUE for A
 prop_validEntailment :: Bool
 prop_validEntailment = entails (Prop 1) (Prop 1)
 
+-- Verifying that A  !--> NOT (A) as per our entailment function
+-- i.e. TRUE for A does NOT imply TRUE for NOT (A)
 prop_invalidEntailment :: Bool
 prop_invalidEntailment = not (entails (Prop 1) (Neg (Prop 1)))
+
+-- note: We tried to perform an entailment test on Prop 1 and Prop 2, but our code does not
+--       seem to like it. We are assuming this happend because the allVals for Prop 1 and Prop 2
+--       have no equivalence. We think that this can however be improved.
 
 
 main :: IO ()
