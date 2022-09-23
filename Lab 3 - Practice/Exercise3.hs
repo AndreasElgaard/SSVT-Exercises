d6790b09be9ad49f7b60278fb75baa50ffe9306c
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2022-09-23 11:14:30 +0200

    [LAB-3] Ex 3 Notes, ex 6

--- Lab 3 - Practice/Exercise3.hs
+++ Lab 3 - Practice/Exercise3.hs
@@ -5,48 +5,53 @@ import           Data.List
 import           Lecture3
 import           Test.QuickCheck
 
-cnf :: Form -> Form
-cnf = nnf . arrowfree
-
--- test :: [Form] -> [Form]
--- test (Neg a : Cnj fs       : xs) = Dsj (map Neg fs) : test xs
--- test (Neg a : Dsj fs       : xs) = Cnj (map Neg fs) : test xs
--- test (Neg a : (Impl f1 f2) : xs) = Cnj [f1, Neg f2] : test xs
--- test []                          = []
--- test (x : xs)                    = x : test xs
-
-
--- CNJ ([Prop 1, DSJ ([Prop 2, Prop 3])])
--- DSJ(CNJ ([Prop 1, Prop 2]), CNJ ([Prop 1, Prop 3])))
-
+-- Applying De Morgan's Law such that all negations apply at atomic level (Prop level).
+-- Refer to https://stackoverflow.com/questions/655261/how-to-convert-a-propositional-formula-to-conjunctive-normal-form-cnf
 deMorgansHandler :: Form -> Form
 deMorgansHandler (Prop x      ) = Prop x
-deMorgansHandler (Neg  (Cnj a)) = Dsj (map (Neg . deMorgansHandler) a)
-deMorgansHandler (Neg  (Dsj a)) = Cnj (map (Neg . deMorgansHandler) a)
+-- By De Morgan's Law: neg (Cnj[a, b]) = Dsj [(neg a), (neg b)]
+deMorgansHandler (Neg  (Cnj a)) = Dsj (map (deMorgansHandler . Neg) a)
+-- By De Morgan's Law: neg (Dsj[a, b]) = Cnj [(neg a), (neg b)]
+deMorgansHandler (Neg  (Dsj a)) = Cnj (map (deMorgansHandler . Neg) a)
 deMorgansHandler (Cnj  a      ) = Cnj (map deMorgansHandler a)
 deMorgansHandler (Dsj  a      ) = Dsj (map deMorgansHandler a)
+-- By De Morgan's Law: neg (Impl (a, b)) = Cnj [a, (neg b)]
 deMorgansHandler (Neg (Impl f1 f2)) =
     Cnj [deMorgansHandler f1, Neg (deMorgansHandler f2)]
 deMorgansHandler (Impl f1 f2) =
     Impl (deMorgansHandler f1) (deMorgansHandler f2)
 deMorgansHandler (Equiv f1 f2) =
     Equiv (deMorgansHandler f1) (deMorgansHandler f2)
-deMorgansHandler x = x
-
-deMorgansHandler1 :: Form -> Form
-deMorgansHandler1 (Prop x) = Prop x
-deMorgansHandler1 (Neg  f) = Neg (deMorgansHandler1 f)
-deMorgansHandler1 (Cnj  a) = Dsj (map (Neg . deMorgansHandler1) a)
-deMorgansHandler1 (Dsj  a) = Cnj (map (Neg . deMorgansHandler1) a)
-deMorgansHandler1 (Impl f1 f2) =
-    Cnj [deMorgansHandler1 f1, Neg (deMorgansHandler1 f2)]
-deMorgansHandler1 x = x
-
--- handler (Cnj a) f1 =
-
--- distributiveLawHandler :: Form -> Form
--- distributiveLawHandler (Cnj a) = map distributiveLawHandler a
--- distributiveLawHandler (Dsj a) = map
+deMorgansHandler (Neg (Neg (Prop a))) = Prop a
+deMorgansHandler (Neg a             ) = Neg (deMorgansHandler a)
 
 
+cnf f1 = conjuctiveNormalForm  where
+    -- Applying De Morgan's Law to convert the below to CNF form.
+    -- Ref. to Q13, SSVT workshop 3
+    -- Refer to https://stackoverflow.com/questions/655261/how-to-convert-a-propositional-formula-to-conjunctive-normal-form-cnf
+    conjuctiveNormalForm    = deMorgansHandler negatedDisjunctedProps
+    -- Disjuncting each combination of props, then applying an outer Negator
+    -- Ref. to Q13, SSVT workshop 3
+    negatedDisjunctedProps  = Neg (Dsj conjunctedProps)
+    -- Conjucting props within each combination
+    -- Ref. to Q13, SSVT workshop 3
+    conjunctedProps         = map Cnj propsMap
+    -- mapping true, false for each prop to have each atomic value as x (true) or -x (false)
+    propsMap                = map createFormForProp propsOfFalseEvaluations
+    -- Only considering the True, False combinations. Not considering output since we
+    -- know output is false for every combination filtered.
+    propsOfFalseEvaluations = map fst falseEvaluations
+    -- Filtering out all True, False combinations which do not evaluate to False with provided form
+    -- Ref. to Q13, SSVT workshop 3
+    falseEvaluations = filter (\(_, bool) -> not bool) truthTableEvaluations
+    -- Getting evaluations of form for all possible True, False combinations for props
+    truthTableEvaluations   = map (\n -> (n, evl n f1)) (allVals f1)
 
+-- each prop to have each atomic value as x (true) or -x (false)
+createFormForProp :: [(Name, Bool)] -> [Form]
+createFormForProp ((prop, True ) : xs) = Prop prop : createFormForProp xs
+createFormForProp ((prop, False) : xs) = Neg (Prop prop) : createFormForProp xs
+createFormForProp []                   = []
+
+-- Time spent: 210 minutes --
