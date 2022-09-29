834704ad7aa38e3f9df1559a348ddaf59a9596a4
agnes.masip <agnes.masip@estudiantat.upc.edu>
2022-09-29 10:14:06 +0200

    theory

---
+++ Lab 4 - Practise/Exercise1-Agnes.hs
@@ -0,0 +1,11 @@
+
+--Invalid IOLTS by definition:
+
+--  Def 1. The list of states and list of labels should be countable (since quickcheck is infinite, a take should be necessary)
+--  Def 1. List of states non empty
+--  Def 1. [LabeledTransition] states are part of the list of states (If you create from [LabelTrans] a LTS, it should be the same as the lts where we obtaiend the [LabelTrans])
+--  Def 1. Last state in LTS is initial state
+--  Def 1. Tau constraints. I am not sure how to deal with them
+
+--  Def 2. A with a set size. Any subsets of A, if we compose them, they are a concatenation
+--  
