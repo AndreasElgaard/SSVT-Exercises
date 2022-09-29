c53103bc4df5e5992055902307b4714b92c15a1d
Ale <mifsud.aleandro@gmail.com>
2022-09-29 23:00:36 +0200

    the generator transitions are now working babyyy sheeeeeeeeeeeesh

--- Lab 4 - Practise/Exercise2-Ale.hs
+++ Lab 4 - Practise/Exercise2-Ale.hs
@@ -1,6 +1,5 @@
 module Exercise2 where
 
-import           Control.Monad.Trans.RWS        ( state )
 import           Data.List
 import           Exercise1
 import           LTS
@@ -8,25 +7,74 @@ import           Test.QuickCheck
 
 genIOLTS :: Gen IOLTS
 genIOLTS = do
-    states      <- genAllStates 5
-    labels      <- genAllLabels 5
-    transitions <- vectorOf 5 (genTrans states labels)
-    return (states, labels, labels, transitions, head states)
+    states    <- genStates 5
+    inLabels  <- genLabels 5 [] '?' 'A' 'M'
+    outLabels <- genLabels 5 inLabels '!' 'N' 'Z'
+    let allLabels = inLabels ++ outLabels
+    initTransitions    <- vectorOf 5 (genTrans states allLabels)
+    checkedTransitions <- makeAllTransPerms states allLabels initTransitions
+    return (states, inLabels, outLabels, checkedTransitions, head states)
 
-genAllStates :: Int -> Gen [State]
-genAllStates vector = vectorOf vector (abs <$> (arbitrary :: Gen Integer))
+genStates :: Int -> Gen [State]
+genStates vector = do
+    listOfStates <- vectorOf vector (abs <$> (arbitrary :: Gen Integer))
+    if checkDupStates listOfStates
+        then return listOfStates
+        else genStates vector
 
-genAllLabels :: Int -> Gen [Label]
-genAllLabels vector = vectorOf vector genChar
+checkDupStates :: [State] -> Bool
+checkDupStates [] = True
+checkDupStates (x : xs) | x `elem` xs = False
+                        | otherwise   = checkDupStates xs
 
-genChar :: Gen Label
-genChar = do
-    a <- elements ['A' .. 'Z']
-    return [a]
+genLabels :: Int -> [Label] -> Char -> Char -> Char -> Gen [Label]
+genLabels vector currentLabels ioType startChar endChar = do
+    listOfChars <- vectorOf vector (genChar ioType startChar endChar)
+    checkDup (checkDupLabels listOfChars)
+             listOfChars
+             vector
+             currentLabels
+             ioType
+             startChar
+             endChar
+
+checkDup
+    :: Bool -> [Label] -> Int -> [Label] -> Char -> Char -> Char -> Gen [Label]
+checkDup True labels _vector _currentLabels _ioType _startChar _endChar = do
+    return labels
+checkDup False _ vector currentLabels ioType startChar endChar =
+    genLabels vector currentLabels ioType startChar endChar
+
+checkDupLabels :: [Label] -> Bool
+checkDupLabels [] = True
+checkDupLabels (x : xs) | x `elem` xs = False
+                        | otherwise   = checkDupLabels xs
+
+genChar :: Char -> Char -> Char -> Gen Label
+genChar ioType startChar endChar = do
+    randomChar <- elements [startChar .. endChar]
+    return (ioType : [randomChar])
 
 getInitState :: [State] -> State
 getInitState states = do
     head states
+
+makeAllTransPerms
+    :: [State] -> [Label] -> [LabeledTransition] -> Gen [LabeledTransition]
+makeAllTransPerms states labels transitions = do
+    let isValid = all (`isStateInTrans` transitions) states
+    if isValid
+        then do
+            return transitions
+        else do
+            newTrans <- vectorOf 1 (genTrans states labels)
+            makeAllTransPerms states labels (newTrans ++ transitions)
+
+
+isStateInTrans :: State -> [LabeledTransition] -> Bool
+isStateInTrans _ [] = False
+isStateInTrans s ((s1, l, s2) : xs) | s == s1 || s == s2 = True
+                                    | otherwise          = isStateInTrans s xs
 
 genTrans :: [State] -> [Label] -> Gen LabeledTransition
 genTrans states labels = do
@@ -34,6 +82,3 @@ genTrans states labels = do
     label <- elements labels
     end   <- elements states
     return (start, label, end)
-
-
-
