f7756e81178c319aca189c78692857cd801951f3
Andreas Elgaard Sørensen <andreas.elgaard@outlook.com>
2022-10-05 16:58:40 +0200

    Added exercise1-Andreas

---
+++ Lab 5 - Practice/Haskell/Exercise1-Andreas.hs
@@ -0,0 +1,30 @@
+module Exercise1 where
+
+import Test.QuickCheck
+import MultiplicationTable
+import Mutation
+
+-- Time spend: x minutes --
+
+-- Q: Write down which types of output are not yet covered by these mutators, and about their weakness/strength.
+-- 1. Reverse the list values.
+-- Weakness: 
+-- Strength: 
+
+-- 2. Multiply values by a fixed random number.
+-- Weakness: 
+-- Strength:
+
+-- 3. Multiply list_1 with a list_2 of same length where list_1 index n is multiplied with list_2 index n.
+-- Weakness: 
+-- Strength:
+
+-- 4. Add 1 to every element in the list.
+-- Weakness: 
+-- Strength:
+
+-- =================================== Implementation ===================================
+--
+
+-- =================================== Props ===================================
+--
