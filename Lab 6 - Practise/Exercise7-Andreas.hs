a4feb9bdcd0c84e48c7b1176649ba98771f0781c
Andreas Elgaard Sørensen <andreas.elgaard@outlook.com>
2022-10-13 13:40:26 +0200

    Changes to exercise 7

--- Lab 6 - Practise/Exercise7-Andreas.hs
+++ Lab 6 - Practise/Exercise7-Andreas.hs
@@ -4,14 +4,16 @@ import Exercise3
 import Exercise5
 import Test.QuickCheck
 
+-- Time spent: 60 minutes --
+
 -- =================================== DISCUSSION ===================================
 -- Q: Is there a difference between the symmetric closure of the transitive closure of a relation R and the transitive closure of the symmetric closure of R ?
 -- A: You can use two approaches to figure out if the symmetric closure of the transitive closure of a relation R is the same.
 -- The first one is to find an example where this is the case.
 -- The second one is to find a counter-example. If a counter-example is found, the statement would be falsified.
 -- To do this we can define a function that checks if the output of symClos and trClos is the same.
--- The test would only test one relation example: [(1,2),(2,3),(3,4),(4,5)]
-type Rel a = [(a, a)]
+-- The property will only test one relation example: [(1,2),(2,3),(3,4),(4,5)]
+-- It would however be possible to auto generate relations using quickCheck, however this would not be very useful since only one counter example is needed to falsify the statement.
 
 relation = [(1, 2), (2, 3), (3, 4), (4, 5)]
 
@@ -24,6 +26,6 @@ main = do
   putStrLn "\n=== Testing if there is a difference between symClos and trClos ===\n"
   quickCheckResult prop_checkTrClosEqualSymClos
 
--- The the property expects that the two operations does not return the same, and the quickCheck test passes.
+-- The property expects that the two operations does not return the same, and the quickCheck test passes.
 -- It can therefore be concluded that it is not true that
 -- the symmetric closure of the transitive closure of a relation R and the transitive closure of the symmetric closure of R is the same and that there is a difference between them.
