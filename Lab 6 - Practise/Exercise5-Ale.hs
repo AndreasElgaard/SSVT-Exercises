05a6e853814695f78e85fdb9004d08976102606d
Ale <mifsud.aleandro@gmail.com>
2022-10-13 13:52:30 +0200

    ex5 code

--- Lab 6 - Practise/Exercise5-Ale.hs
+++ Lab 6 - Practise/Exercise5-Ale.hs
@@ -1,21 +1,38 @@
 module Exercise5 where
 
 import Exercise3 ( Rel )
-import Data.List(nub)
+import Data.List(nub, elemIndex)
 
 infixr 5 @@
 (@@) :: Eq a => Rel a -> Rel a -> Rel a
 r @@ s =
   nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
 
-trClos ::  Ord a => Rel a -> Rel a
-trClos rel = rel
+
+trClos :: Eq a => Rel a -> Rel a
+trClos = iterateWIndex 0
+
+-- findAndReplaceInList inTuple fullList =
+
+iterateWIndex :: Eq a => Int -> Rel a -> Rel a
+iterateWIndex counter [] = []
+iterateWIndex counter xy@(x:xs)
+  | (counter + 1) == length xy = checkTuple' xy
+  | otherwise =  checkTuple' xy ++ iterateWIndex (counter+1) (move (counter+1) xy)
 
 
-mapThrough [] = []
-mapThrough ((a,b):xs) = map (\(x,y) -> checkTuple (a,b) (x,y)) xs
+checkTuple' :: Eq a => Rel a -> Rel a
+checkTuple' [] = []
+checkTuple' ((x1,x2):(y1,y2):xs)
+  | x2 == y1 = (x1,y2) : checkTuple' ((x1,x2):xs)
+  | otherwise = checkTuple' ((x1,x2):xs)
+checkTuple' ((x1,x2):xs)
+  | null xs = []
+  | x2 == y1 = (x1,y2) : checkTuple' ((x1,x2):[])
+  | otherwise = checkTuple' ((x1,x2):[])
+      where (y1,y2) = head xs
 
-checkTuple :: (Int, Int) -> (Int, Int) -> (Int, Int)
-checkTuple (a,b) (c,d)
-  | b == c = (a,d)
-  | otherwise = (undefined, undefined)
+-- Taken from https://stackoverflow.com/questions/1041440/how-to-move-an-element-in-a-list-in-haskell
+move :: Int -> [a] -> [a]
+move n as = head ts : (hs ++ tail ts)
+   where (hs, ts) = splitAt n as
