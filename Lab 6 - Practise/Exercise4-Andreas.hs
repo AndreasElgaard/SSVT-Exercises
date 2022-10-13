75271c3be0107738014eaa7d94c32d71ea157c61
agnes.masip <agnes.masip@estudiantat.upc.edu>
2022-10-13 15:27:42 +0200

    main folder

--- Lab 6 - Practise/Exercise4-Andreas.hs
+++ Lab 6 - Practise/Exercise4-Andreas.hs
@@ -14,11 +14,18 @@ import Test.QuickCheck
 type Rel a = [(a, a)]
 
 isSerial :: Eq a => [a] -> Rel a -> Bool
-isSerial domain relation = isSerialHelper domain domain relation
+isSerial domain relation = 
+    
+    
+    
+    
+    isSerialHelper domain domain relation
 
 isSerialHelper [] _ _ = True
-isSerialHelper (x : xs) domain relation = if secondHelper x domain relation then isSerialHelper xs domain relation else False
+isSerialHelper (x : xs) relation = 
+        |inSet x relation 
 
+        Set a
 -- =================================== TEST ===================================
 -- Property that checks if the length of relations is smaller than the domain.
 -- If it is smaller then that means that they are not serial.
