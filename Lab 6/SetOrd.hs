6fd13c152ecbe6742f395cbb20c8c8e7660acff8
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2022-10-14 10:09:09 +0200

    [LAB-6] SetOrd updates

--- Lab 6/SetOrd.hs
+++ Lab 6/SetOrd.hs
@@ -1,78 +1,95 @@
-module SetOrd (Set(..),emptySet,isEmpty,inSet,subSet,insertSet,
-               deleteSet,powerSet,takeSet,(!!!),list2set,unionSet) 
+module SetOrd
+   ( Set(..)
+   , emptySet
+   , isEmpty
+   , inSet
+   , inSetEq
+   , subSet
+   , insertSet
+   , deleteSet
+   , powerSet
+   , takeSet
+   , (!!!)
+   , list2set
+   , unionSet
+   ) where
 
-where
+import           Data.List                      ( sort )
 
-import Data.List (sort) 
-
-{-- Sets implemented as ordered lists without duplicates --} 
+{-- Sets implemented as ordered lists without duplicates --}
 
 newtype Set a = Set [a] deriving (Eq,Ord)
 
 instance (Show a) => Show (Set a) where
-    showsPrec _ (Set s) str = showSet s str
+   showsPrec _ (Set s) str = showSet s str
 
-showSet []     str = showString "{}" str
-showSet (x:xs) str = showChar '{' ( shows x ( showl xs str))
-     where showl []     str = showChar '}' str
-           showl (x:xs) str = showChar ',' (shows x (showl xs str))
+showSet []       str = showString "{}" str
+showSet (x : xs) str = showChar '{' (shows x (showl xs str))
+ where
+  showl []       str = showChar '}' str
+  showl (x : xs) str = showChar ',' (shows x (showl xs str))
 
-emptySet  :: Set a       
+emptySet :: Set a
 emptySet = Set []
 
-isEmpty  :: Set a -> Bool            
+isEmpty :: Set a -> Bool
 isEmpty (Set []) = True
 isEmpty _        = False
 
-inSet  :: (Ord a) => a -> Set a -> Bool  
+inSet :: (Ord a) => a -> Set a -> Bool
 inSet x (Set s) = elem x (takeWhile (<= x) s)
 
 subSet :: (Ord a) => Set a -> Set a -> Bool
-subSet (Set []) _       = True  
-subSet (Set (x:xs)) set = (inSet x set) && subSet (Set xs) set 
+subSet (Set []      ) _   = True
+subSet (Set (x : xs)) set = (inSet x set) && subSet (Set xs) set
 
-insertSet :: (Ord a) => a -> Set a -> Set a 
-insertSet x (Set s) = Set (insertList x s) 
+inSetEq :: (Eq a) => a -> Set a -> Bool
+inSetEq x (Set s) = x `elem` s
 
-insertList x [] = [x]
-insertList x ys@(y:ys') = case compare x y of 
-                                 GT -> y : insertList x ys' 
-                                 EQ -> ys 
-                                 _  -> x : ys 
+subSetEq :: (Eq a) => Set a -> Set a -> Bool
+subSetEq (Set []      ) _   = True
+subSetEq (Set (x : xs)) set = (inSetEq x set) && subSetEq (Set xs) set
 
-deleteSet :: Ord a => a -> Set a -> Set a 
+insertSet :: (Ord a) => a -> Set a -> Set a
+insertSet x (Set s) = Set (insertList x s)
+
+insertList x []           = [x]
+insertList x ys@(y : ys') = case compare x y of
+   GT -> y : insertList x ys'
+   EQ -> ys
+   _  -> x : ys
+
+deleteSet :: Ord a => a -> Set a -> Set a
 deleteSet x (Set s) = Set (deleteList x s)
 
-deleteList x [] = []
-deleteList x ys@(y:ys') = case compare x y of 
-                                 GT -> y : deleteList x ys'
-                                 EQ -> ys'
-                                 _  -> ys
+deleteList x []           = []
+deleteList x ys@(y : ys') = case compare x y of
+   GT -> y : deleteList x ys'
+   EQ -> ys'
+   _  -> ys
 
 list2set :: Ord a => [a] -> Set a
-list2set [] = Set []
-list2set (x:xs) = insertSet x (list2set xs)
+list2set []       = Set []
+list2set (x : xs) = insertSet x (list2set xs)
+
 -- list2set xs = Set (foldr insertList [] xs)
 
 powerSet :: Ord a => Set a -> Set (Set a)
-powerSet (Set xs) = 
-   Set (sort (map (\xs -> (list2set xs)) (powerList xs)))
+powerSet (Set xs) = Set (sort (map (\xs -> (list2set xs)) (powerList xs)))
 
-powerList  :: [a] -> [[a]]
-powerList  [] = [[]]
-powerList  (x:xs) = (powerList xs) 
-                     ++ (map (x:) (powerList xs))
+powerList :: [a] -> [[a]]
+powerList []       = [[]]
+powerList (x : xs) = (powerList xs) ++ (map (x :) (powerList xs))
 
 takeSet :: Eq a => Int -> Set a -> Set a
-takeSet n (Set xs) = Set (take n xs) 
+takeSet n (Set xs) = Set (take n xs)
 
 infixl 9 !!!
 
-(!!!) :: Eq a => Set a -> Int -> a 
+(!!!) :: Eq a => Set a -> Int -> a
 (Set xs) !!! n = xs !! n
 
-unionSet :: (Ord a) => Set a -> Set a -> Set a 
-unionSet (Set [])     set2  =  set2
-unionSet (Set (x:xs)) set2  = 
-   insertSet x (unionSet (Set xs) set2)
+unionSet :: (Ord a) => Set a -> Set a -> Set a
+unionSet (Set []      ) set2 = set2
+unionSet (Set (x : xs)) set2 = insertSet x (unionSet (Set xs) set2)
 
