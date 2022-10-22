71a351f19a6c92e59d5c6573c637f387e50c2f22
Luke Bezzina <luke.bezzina.18@um.edu.mt>
2022-10-22 12:27:55 +0200

    [HELPER]

--- helper.hs
+++ helper.hs
@@ -14,3 +14,14 @@ hoareTest precondition f postcondition =
 stronger, weaker :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
 stronger xs p q = forall xs (\x -> p x --> q x)
 weaker xs p q = stronger xs q p
+
+type Rel a = [(a, a)]
+
+isTransitive :: Eq a => Rel a -> Bool
+isTransitive x = and [ (a, d) `elem` x | (a, b) <- x, (c, d) <- x, b == c ]
+
+isReflexive :: Eq a => Rel a -> [a] -> Bool
+isReflexive r = all (\x -> (x, x) `elem` r)
+
+isAntisymmetric :: Eq a => Rel a -> Bool
+isAntisymmetric r = and [ (y, x) `notElem` r || x == y | (x, y) <- r ]
